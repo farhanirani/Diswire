7cd61678c8d69a37778f1cc4eec24042349173e4
Farhan Irani <farhanirani007@gmail.com>
2020-11-07 23:24:36 +0530

    messages routes for groups and personal chat post get done

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,8 +1,93 @@
+const jwt = require("jsonwebtoken");
+
 //========================================================================================
 /*                                                                                      *
- *                              Send personal message
+ *                              Get personal chat
  *                                                                                      */
 //========================================================================================
+
+module.exports.getPersonalChat = async (req, res) => {
+  const db = req.app.locals.db;
+
+  try {
+    const token = req.header("x-auth-token");
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) return res.json(false);
+    id1 = verified.id;
+    id2 = req.params.id;
+
+    const querydata = await db.query(
+      "SELECT * FROM messages_personal WHERE (m_sender_id = ? AND m_reciever_id = ?) OR (m_sender_id = ? AND m_reciever_id = ?)",
+      [id1, id2, id2, id1]
+    );
+    res.status(200).json(querydata[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//========================================================================================
+/*                                                                                      *
+ *                              Get group chat
+ *                                                                                      */
+//========================================================================================
+
+module.exports.getGroupChat = async (req, res) => {
+  const db = req.app.locals.db;
+
+  try {
+    const token = req.header("x-auth-token");
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) return res.json(false);
+    userid = verified.id;
+    groupid = req.params.id;
+
+    const checkIfUserInGroup = await db.query(
+      "SELECT * FROM group_connections WHERE userid = ? AND groupid = ?",
+      [userid, groupid]
+    );
+
+    console.log(checkIfUserInGroup[0][0]);
+    if (!checkIfUserInGroup[0][0]) {
+      res.status(401).json("Not authorized!!!!!!!!");
+    } else {
+      const querydata = await db.query(
+        "SELECT * FROM messages_group WHERE m_group_id = ?",
+        [groupid]
+      );
+      res.status(200).json(querydata[0]);
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//========================================================================================
+/*                                                                                      *
+ *                              Post personal message
+ *                                                                                      */
+//========================================================================================
+
+module.exports.postPersonalChat = async (req, res) => {
+  const db = req.app.locals.db;
+
+  try {
+    const { messagedata } = req.body;
+    const token = req.header("x-auth-token");
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) return res.json(false);
+    id1 = verified.id;
+    id2 = req.params.id;
+
+    const querydata = await db.query(
+      "INSERT INTO messages_personal(m_body, m_sender_id, m_reciever_id) VALUES (?,?,?)",
+      [messagedata, id1, id2]
+    );
+    res.status(200).json({ message: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 //========================================================================================
 /*                                                                                      *
@@ -10,8 +95,34 @@
  *                                                                                      */
 //========================================================================================
 
-//========================================================================================
-/*                                                                                      *
- *                              Check token
- *                                                                                      */
-//========================================================================================
+module.exports.postGroupChat = async (req, res) => {
+  const db = req.app.locals.db;
+
+  try {
+    const token = req.header("x-auth-token");
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) return res.json(false);
+
+    const { messagedata } = req.body;
+    userid = verified.id;
+    groupid = req.params.id;
+
+    const checkIfUserInGroup = await db.query(
+      "SELECT * FROM group_connections WHERE userid = ? AND groupid = ?",
+      [userid, groupid]
+    );
+
+    // console.log(checkIfUserInGroup[0][0]);
+    if (!checkIfUserInGroup[0][0]) {
+      res.status(401).json("Not authorized!!!!!!!!");
+    } else {
+      const querydata = await db.query(
+        "INSERT INTO messages_group(m_body, m_sender_id, m_group_id) VALUES (?,?,?)",
+        [messagedata, userid, groupid]
+      );
+      res.status(200).json({ message: "success" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
--- backend/routes.js
+++ backend/routes.js
@@ -29,9 +29,9 @@ router.post("/user/checkToken", User.checkToken);
 
 const Message = require("./controllers/messageController");
 
-// SELECT * FROM messages_personal WHERE (m_sender_id = 1 AND m_reciever_id = 2) OR (m_sender_id = 2 AND m_reciever_id = 1)
-// router.get("/messages/personal/:id", Message.getPersonalChat);
-// SELECT * FROM messages_group WHERE m_group_id = 1
-// router.get("/messages/group/:id", Message.getGroupChat);
+router.get("/messages/personal/:id", auth, Message.getPersonalChat);
+router.get("/messages/group/:id", auth, Message.getGroupChat);
+router.post("/messages/post/:id", auth, Message.postPersonalChat);
+router.post("/messages/postgroup/:id", auth, Message.postGroupChat);
 
 module.exports = router;
