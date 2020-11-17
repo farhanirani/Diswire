f3050b36e1774b6422eb5850b3041e4d16bfd0a3
Farhan Irani <farhanirani007@gmail.com>
2020-11-17 14:21:37 +0530

    client

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use("/api", routes);
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   // Handle React routing, return all requests to React app
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 
