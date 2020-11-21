079a68da35ac2ef0a60a79dc803d24df5030fdf2
vincent2528 <krutikgambhir@gmail.com>
2020-11-21 16:41:28 +0530

    member remove

--- client/src/Components/MemberList.js
+++ client/src/Components/MemberList.js
@@ -5,6 +5,9 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import Popup from "reactjs-popup";
+import MeetingRoomTwoToneIcon from "@material-ui/icons/MeetingRoomTwoTone";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function MemberList() {
   const history = useHistory();
@@ -44,11 +47,34 @@ function MemberList() {
                 history.push("/channels/@me/" + member.userid);
               }}
             >
-              <Avatar
-                style={{ height: "30px", width: "30px" }}
-                src={member.profile_pic}
-              />
-              <h3>{member.username}</h3>
+              <div className="member-info-left">
+                <Avatar
+                  style={{ height: "30px", width: "30px" }}
+                  src={member.profile_pic}
+                />
+                <h3>{member.username}</h3>
+              </div>
+              <div className="member-info-right">
+                <Popup
+                  trigger={<MoreVertIcon className="profileicons" />}
+                  position="bottom right"
+                  closeOnDocumentClick
+                  mouseLeaveDelay={300}
+                  mouseEnterDelay={0}
+                  contentStyle={{ padding: "0px", border: "none" }}
+                  arrow={false}
+                >
+                  <div className="menu">
+                    <div className="menu-item-leave">
+                      Remove User
+                      <MeetingRoomTwoToneIcon
+                        style={{ paddingTop: "2px" }}
+                        fontSize="small"
+                      />
+                    </div>
+                  </div>
+                </Popup>
+              </div>
             </div>
           );
         })}
